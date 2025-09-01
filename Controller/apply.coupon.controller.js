0daddaf9ae6e60b50414cce4250a40ae31461b30
abhay-singh9584 <as763829@gmail.com>
2025-09-01 11:15:03 +0530

    Task Completed

--- Controller/apply.coupon.controller.js
+++ Controller/apply.coupon.controller.js
@@ -1,17 +1,197 @@
+const CouponModel = require("../Model/Coupon.model");
+  
 const applicableCoupons = async(req,res)=>{
     try {
+        const { cart } = req.body;
+
+        if (!cart || !cart.items || cart.items.length === 0) {
+            return res.status(400).json({ message: "Cart is empty or invalid" });
+        }
+
+        const cartTotal = calculateCartTotal(cart);
+
+        const coupons = await CouponModel.find({ active: true });
+        if(!coupons){
+            return res.status(404).json({ message: "No coupons found" });
+        }
+
+        let applicableCoupons = [];
         
+        for(let coupon of coupons){
+            let discount = 0;
+
+            if(coupon.type === "cart-wise" && cartTotal >= coupon.details.threshold){
+                discount = (cartTotal * coupon.details.discount)/100;
+            }
+
+            if(coupon.type === "product-wise"){
+                const product = cart.items.find((i)=>{
+                    return i.product_id == coupon.details.product_id
+                })
+                if(product){
+                    discount = (product.price * coupon.details.discount)/100;
+                }
+            }
+
+            if(coupon.type === "bxgy"){
+                let count = 0;
+                coupon.details.buy_products.forEach((product)=>{
+                    const item = cart.items.find((i)=>{
+                        return i.product_id === product.product_id
+                    })
+                    if(item){
+                        count += Math.floor(item.quantity/product.quantity);
+                    }
+                })
+                const repetition =  coupon.details.repetition_limit &&
+                count > coupon.details.repetition_limit
+                  ? coupon.details.repetition_limit
+                  : count;
+                  if (repetition > 0) {
+                    // For each repetition, add discount equal to "get" products price
+                    coupon.details.get_products.forEach((get) => {
+                      const productInCart = cart.items.find((i) => 
+                        i.product_id === get.product_id
+                      );
+                      if (productInCart) {
+                        discount += productInCart.price * get.quantity * (repetition-1);
+                      }
+                    });
+                }
+                
+            }
+            if(discount > 0){
+                applicableCoupons.push({
+                    code:coupon.code,
+                    type:coupon.type,
+                    discount:discount
+                })
+            }
+
+        }
+        
+        return res.status(200).json({message:"Applicable coupons fetched successfully",applicableCoupons});
+
     } catch (error) {
-        
+        console.log(error);
+        return res.status(500).json({message:"Internal server error",error});
     }
 }
 
 const applyCoupon = async(req,res)=>{
     try {
+        const { id } = req.params;
+        const { cart } = req.body;
+
+        if (!cart || !cart.items || cart.items.length === 0 || !id) {
+        return res.status(400).json({ message: "Cart is empty, invalid or coupon code is missing" });
+        }
+
+        const coupon = await CouponModel.findOne({code:id});
+        if(!coupon){
+            return res.status(404).json({ message: "No coupon found" });
+        }
+
+        let totalDiscount = 0;
         
+        const cartTotal = calculateCartTotal(cart);
+
+        if (coupon.type === "cart-wise") {
+            if (cartTotal >= coupon.details.threshold) {
+                totalDiscount = (cartTotal * coupon.details.discount) / 100;
+            }
+        }
+
+        if (coupon.type === "product-wise") {
+            updatedItems = updatedItems.map((item) => {
+                if (item.product_id == coupon.details.product_id) {
+                    const discount =
+                    (item.price * item.quantity * coupon.details.discount) / 100;
+                    totalDiscount += discount;
+                    return { ...item, total_discount: discount };
+                }
+                return item;
+            });
+        }
+
+        if (coupon.type === "bxgy") {
+            let buyCount = 0;
+
+            cart.items.forEach((item) => {
+                const inBuyPool = coupon.details.buy_products.find(
+                  (bp) => bp.product_id == item.product_id
+                );
+                if (inBuyPool) {
+                  buyCount += item.quantity;
+                }
+              });
+
+            const sets = Math.floor(buyCount / coupon.details.buy_quantity);
+
+            const repetition = Math.min(
+              sets,
+              coupon.details.repetition_limit ?? Infinity
+            );
+
+            if (repetition > 0) {
+                updatedItems = updatedItems.map((item) => {
+                    const gp = coupon.details.get_products.find(
+                      (g) => g.product_id == item.product_id
+                    );
+                    if (gp) {
+                        const freeQty = gp.quantity * repetition;
+                        const discount = item.price * freeQty;
+                        totalDiscount += discount;
+
+                        return {
+                            ...item,
+                            quantity: item.quantity + freeQty,
+                            total_discount: discount
+                        };
+                    }
+                    return item;
+                });
+
+                coupon.details.get_products.forEach((gp) => {
+                    const exists = updatedItems.find(
+                        (i) => i.product_id == gp.product_id
+                    );
+                    if (!exists) {
+                        updatedItems.push({
+                            product_id: gp.product_id,
+                            quantity: gp.quantity * repetition,
+                            price: 0,
+                            total_discount: 0
+                        });
+                    }
+                });
+            }
+        }
+        let updatedItems = cart.items.map((item) => ({
+          ...item,
+          total_discount: 0
+        }));
+
+
+        return res.status(200).json({ updated_cart: {
+            items: updatedItems,
+            total_price: cartTotal,
+            total_discount: totalDiscount,
+            final_price: cartTotal - totalDiscount
+        } });
+
     } catch (error) {
-        
+        console.log(error);
+        return res.status(500).json({message:"Internal server error",error});
     }
+}
+
+const calculateCartTotal = (cart) => {
+    let total = 0;
+    for (const item of cart.items) {
+        total += item.price * item.quantity;
+    }
+    return total;
 }
 
 module.exports = {
--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # Monk_Task
+
+This project implements a **RESTful API** for managing and applying discount coupons (cart-wise, product-wise, and BxGy) for an e-commerce platform.  
+Built with **Node.js, Express, MongoDB, and Mongoose**.
+
+# Installation & Setup
+
+1. Clone the repo:
+   ```bash
+   git clone https://github.com/abhay-singh9584/Monk_Task.git
+   cd Monk_Task
+   ```
+2. Install dependencies:
+   ```bash
+   npm install
+   ```
+3. Setup `config.js`:
+   ```js
+   PORT=8000
+   MONGO_URI=mongodb://localhost:27017/Monk
+   ```
+4. Start server:
+   ```bash
+   npm run dev
+
+# API Endpoints
+
+1. Coupon Management (CRUD)
+   - POST /coupons → Create coupon  
+   - GET /coupons → Get all coupons  
+   - GET /coupons/:id → Get coupon by ID  
+   - PUT /coupons/:id → Update coupon  
+   - DELETE /coupons/:id → Delete coupon  
+
+2. Coupon Application
+   - POST /applicable-coupons → Fetch all applicable coupons for a given cart and their discounts  
+   - POST /apply-coupon/:id → Apply a specific coupon to cart and return updated cart  
+
+
+# Implemented Cases
+
+   1. Cart-Wise Coupon
+   2. Product-Wise Coupon
+   3. BxGy Coupon
+
+# Unimplemented Cases
+
+   1. A seprate error handler, I don't implement it because its not making sense in this case.
+   2. I have implemented the Service layer as it can be done in Controller layer.
+
+# Assumptions
+
+   1. In applicabe coupon api, in your response product-wise coupon was not applied so i added it.
+   2. Also in your's api there were some wrong calculations so i fixed it.
+
+# Author
+Backend API by **Abhay Singh**
